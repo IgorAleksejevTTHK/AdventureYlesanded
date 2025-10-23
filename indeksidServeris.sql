58cb4f8c5e338ee5e624db474ff93215b4f282cc
IgorAleksejevTTHK <igoralekseje@gmail.com>
2025-10-23 15:32:50 +0300

    lisatud rohkem kommentaare

--- indeksidServeris.sql
+++ indeksidServeris.sql
@@ -1,87 +1,92 @@
 
-select * from dimemployee
+SELECT * FROM dimemployee;
 
-create index ix_dimemployee_baserate
-on dimemployee (baserate asc)
+-- Loome indeksi ix_dimemployee_baserate veerule BaseRate tõusvas järjekorras
+CREATE INDEX ix_dimemployee_baserate
+ON dimemployee (baserate ASC);
 
+-- Kuvame tabeli dimemployee struktuuri (veerud, andmetüübid jms)
+EXEC sp_help dimemployee;
 
-exec sp_help dimemployee
-
-
-drop index dimemployee.ix_dimemployee_baserate
-
--- loome clustered indeksi firstname veerule tabelis tblemployee
-create clustered index ix_tblemployee_name
-on tblemployee (firstname)
-
-drop index tblemployee.pk__tblemplo__3214ec070a9d95db
-
--- loome clustered indeksi mitme veeruga
-create clustered index ix_tblemployee_gender_salary
-on tblemployee (gender desc, salary asc)
-
--- loome nonclustered indeksi firstname veerule
-create nonclustered index ix_tblemployee_name
-on tblemployee (firstname)
+-- Kustutame indeksi ix_dimemployee_baserate tabelist dimemployee
+DROP INDEX dimemployee.ix_dimemployee_baserate;
 
 
-exec sp_helpindex dimemployee
+-- Loome klasterindeksi veerule FirstName tabelis tblemployee
+-- (klasterindeks määrab reaalse andmete järjestuse tabelis)
+CREATE CLUSTERED INDEX ix_tblemployee_name
+ON tblemployee (firstname);
 
--- loome unikaalse nonclustered indeksi kahe veeru kombinatsioonile
-create unique nonclustered index uix_dimemployee_firstname_lastname
-on dimemployee (firstname, lastname)
-
--- lisame tabelile unikaalse constrainti veerule title
-alter table dimemployee 
-add constraint uq_dimemployee_title
-unique nonclustered (title)
+-- Kustutame olemasoleva klasterindeksi
+DROP INDEX tblemployee.pk__tblemplo__3214ec070a9d95db;
 
 
-execute sp_helpconstraint dimemployee
-
--- loome unikaalse indeksi, ignoreerib dubleeritud väärtusi
-create unique index ix_dimemployee_title
-on dimemployee (title)
-with ignore_dup_key
+-- Loome klasterindeksi mitmele veerule:
+-- esmalt Gender kahanevas järjekorras, seejärel Salary tõusvas järjekorras
+CREATE CLUSTERED INDEX ix_tblemployee_gender_salary
+ON tblemployee (gender DESC, salary ASC);
 
 
-select * from dimemployee
-where baserate > 5 and baserate < 10
+-- Loome mitte-klasterindeksi veerule FirstName
+CREATE NONCLUSTERED INDEX ix_tblemployee_name
+ON tblemployee (firstname);
 
 
-delete from dimemployee
-where baserate = 9.50
+-- Kuvame kõik tabeli dimemployee indeksid
+EXEC sp_helpindex dimemployee;
 
 
-update dimemployee
-set baserate = 50
-where baserate = 9.25
+-- Loome unikaalse mitte-klasterindeksi kahe veeru (FirstName, LastName) kombinatsioonile
+CREATE UNIQUE NONCLUSTERED INDEX uix_dimemployee_firstname_lastname
+ON dimemployee (firstname, lastname);
 
 
-select * from dimemployee
+-- Lisame tabelile unikaalse piirangu (constraint) veerule Title
+
+ALTER TABLE dimemployee 
+ADD CONSTRAINT uq_dimemployee_title
+UNIQUE NONCLUSTERED (title);
 
 
-select * from dimemployee
-order by baserate
+-- Kuvame kõik tabeli dimemployee piirangud 
+EXECUTE sp_helpconstraint dimemployee;
 
-select * from dimemployee
-order by baserate desc
 
--- loendame töötajaid iga baserate väärtuse järgi
-select baserate, count(baserate) as total
-from dimemployee
-group by baserate
-SELECT * FROM DimEmployee WHERE BaseRate > 5 AND BaseRate < 10
+-- Loome unikaalse indeksi veerule Title,
+-- mis ignoreerib dubleeritud väärtusi 
+CREATE UNIQUE INDEX ix_dimemployee_title
+ON dimemployee (title)
+WITH IGNORE_DUP_KEY;
 
-DELETE FROM DimEmployee WHERE BaseRate = 9.50
-UPDATE DimEmployee SET BaseRate = 50 WHERE BaseRate = 9.25
 
-SELECT * FROM DimEmployee
+-- Valime kõik töötajad, kelle BaseRate on vahemikus 5 kuni 10
+SELECT * FROM dimemployee
+WHERE baserate > 5 AND baserate < 10;
 
-SELECT * FROM DimEmployee ORDER BY BaseRate
 
-SELECT * FROM DimEmployee ORDER BY BaseRate DESC
+-- Kustutame töötajad, kelle BaseRate = 9.50
+DELETE FROM dimemployee
+WHERE baserate = 9.50;
 
-SELECT BaseRate, COUNT(BaseRate) AS Total
-FROM DimEmployee
-GROUP BY BaseRate
+-- Uuendame töötajate BaseRate väärtuseks 50, kui praegune väärtus on 9.25
+UPDATE dimemployee
+SET baserate = 50
+WHERE baserate = 9.25;
+
+
+SELECT * FROM dimemployee;
+
+-------------------------------------------------------
+-- Järjestame töötajad BaseRate järgi kasvavas järjekorras
+SELECT * FROM dimemployee
+ORDER BY baserate;
+
+-- Järjestame töötajad BaseRate järgi kahanevas järjekorras
+SELECT * FROM dimemployee
+ORDER BY baserate DESC;
+
+
+-- Loendame töötajate arvu iga BaseRate väärtuse järgi
+SELECT baserate, COUNT(baserate) AS total
+FROM dimemployee
+GROUP BY baserate;
